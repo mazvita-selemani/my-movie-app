498e153e9def27d57a43064ebe0dac4f86975f8e
Mazvita Selemani <mazvita.selemani@ecoonline.com>
2023-12-10 00:29:59 +0000

    Made first react app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useEffect} from 'react';
 import './styles.css';
 import searchIcon from './search.svg';
 
-//3decaa0d
-//going to use an extrenal api, which will give information about the movies
+import MovieCard from './MovieCard';
+
+//going to use an external api, which will give information about the movies
 
 const API_URL = ' http://www.omdbapi.com/?i=tt3896198&apikey=3decaa0d';
 const App = () => {
 
+    //you can have multiple states or even multiple use effect hooks, there's no limit
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('')
     const searchMovies = async (title) => {
         //this will call the API
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        //renders movies from the search
+        setMovies(data.Search); //console.log to view movie object in console
     };
 
     //using this as static data to render out something to know what we're working with
-    const movie = {
+    // this has been replaced by the useState()
+    /*const movie = {
 
-            "Title": "Italian Spiderman",
-            "Year": "2007",
-            "imdbID": "tt2705436",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
+        "Title": "Italian Spiderman",
+        "Year": "2007",
+        "imdbID": "tt2705436",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BZWQxMjcwNjItZjI0ZC00ZTc4LWIwMzItM2Q0YTZhNzI3NzdlXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_SX300.jpg"
 
-    }
+    };*/
 
     useEffect(() => {
         searchMovies('Spiderman');
@@ -38,29 +44,33 @@ const App = () => {
             <div className="search">
                 <input
                     placeholder="Search for movies"
-                    value="Superman"
-                    onChange={() => {}}
+                    value={searchTerm}
+                    onChange={(event) => {setSearchTerm(event.target.value)}}
                 />
                 <img
-                src={searchIcon}
-                alt="search"
-                onChange={() => {}}
+                    src={searchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
                 />
             </div>
 
-            <div className="container">
-                <div>
-                    <p>{movie.Year}</p>
-                </div>
+            {
+                movies.length > 0 //if
+                    ? (
+                        <div className="container">
+                            {/* <MovieCard movie={movies[9]}/> */ }
+                            {movies.map((movie) => <MovieCard movie={movie}/>)}
+                        </div>
+                    ) : //else
+                    (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    )
 
-                <div>
-                    <img
-                        src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'}
-                        alt={movie.Title}
+            }
 
-                    />
-                </div>
-            </div>
+
         </div>
     );
 
